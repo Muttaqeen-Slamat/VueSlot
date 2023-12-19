69cb17ccacf37f4ba74cd2a4bf733a93b33a7029
Muttaqeen-Slamat <muttaqeens@gmail.com>
2023-12-19 08:41:58 +0200

    patch from sir

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
-<template>
-  <!-- <Card>
+<!--  <template>
+   <Card>
     <div class="card">
       <p>Hello There</p>
     </div>
-  </Card> -->
+  </Card>
   <Card>
     <template v-slot="cardHeader">
-<!-- #cardHeader -->
+ #cardHeader 
       <p class="lead">Title 1</p>
     </template>
     <template v-slot="cardBody">
@@ -45,5 +45,33 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+</style>  -->
+
+
+<template>
+  <div class="card">
+   <div class="card-header">
+       <slot name="cardHeader"></slot>
+   </div>
+   <div class="card-body">
+       <slot name="cardBody"></slot>
+   </div>
+   <div class="card-footer">
+       <slot></slot>
+   </div>
+  </div>
+</template>
+
+<script>
+   export default {
+       name: 'CardComp'
+   }
+</script>
+
+<style scoped>
+.card {
+   width: 18rem;
+   margin: .5rem;
 }
 </style>
